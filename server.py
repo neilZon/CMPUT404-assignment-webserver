97b01852de2893d0a0a86fa122823b2503a3080c
Neilzon <neilcviloria@gmail.com>
2023-01-19 10:17:55 -0700

    basic path stuff

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -26,14 +27,49 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+METHOD = 0
+PATH = 1
+ROOT = "./www"
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        try:
+            self.data = self.request.recv(1024).strip()
+            print ("Got a request of: %s\n" % self.data)
 
+            request_string_components = self.data.decode("utf-8").split(" ")
+
+            if request_string_components[METHOD] != "GET":
+                self.request.sendall(bytearray("HTTP/1.1 405 Method Not Allowed\r\n\r\n",'utf-8'))
+                return
+
+            data = ""
+            content_type = ""
+
+            path = ROOT + request_string_components[PATH]
+            if os.path.exists(path):
+                print("it sexisjfs")
+                if os.path.isdir(path):
+                    files = os.listdir(path)
+                    if "index.html" not in resources:
+                        self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\n\r\n",'utf-8'))
+                        return 
+                    data = "READ FILE and determien file type"
+                elif os.path.isfile(path):
+                    # serve file
+                    print("\nIt is a normal file")  
+
+            resources = os.listdir(ROOT + request_string_components[PATH])
+
+            print(resources)
+
+            self.request.sendall(bytearray(f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html></html>",'utf-8'))
+        except Exception as e:
+            print(e)
+            self.request.sendall(bytearray("HTTP/1.1 500 Internal Server Error\r\n\r\n",'utf-8'))
+
+ 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
