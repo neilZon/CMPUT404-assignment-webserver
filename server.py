f9dd32db0d32d7a77f203008cf4bb1bf9bdaccaa
Neilzon <neilcviloria@gmail.com>
2023-01-24 12:50:01 -0700

    even more stuff

--- server.py
+++ server.py
@@ -27,47 +27,95 @@ import os
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+# constants
 METHOD = 0
 PATH = 1
 ROOT = "./www"
+
+HTTP_200_STATUS = "HTTP/1.1 200 OK\r\n"
+HTTP_404_STATUS = "HTTP/1.1 404 Not Found\r\n"
+HTTP_405_STATUS = "HTTP/1.1 405 Method Not Allowed\r\n"
+HTTP_500_STATUS = "HTTP/1.1 500 Internal Server Error\r\n"
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         try:
             self.data = self.request.recv(1024).strip()
-            print ("Got a request of: %s\n" % self.data)
+            # print ("Got a request of: %s\n" % self.data)
 
             request_string_components = self.data.decode("utf-8").split(" ")
 
             if request_string_components[METHOD] != "GET":
-                self.request.sendall(bytearray("HTTP/1.1 405 Method Not Allowed\r\n\r\n",'utf-8'))
+                self.request.sendall(bytearray(f"{HTTP_405_STATUS}\r\n",'utf-8'))
                 return
 
             data = ""
             content_type = ""
+            content_encoding = ""
+            content_len = ""
 
             path = ROOT + request_string_components[PATH]
+
+            # clean path of parent accesses
+            path = path.replace("../", "")
+            
             if os.path.exists(path):
-                print("it sexisjfs")
+
                 if os.path.isdir(path):
-                    files = os.listdir(path)
-                    if "index.html" not in resources:
-                        self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\n\r\n",'utf-8'))
-                        return 
-                    data = "READ FILE and determien file type"
+                    files = set(os.listdir(path))
+                    content_type = ""
+                    data = ""
+
+                    if "index.html" in files:
+                        f = open(path + "index.html", "r")
+                        content_type = "text/html"
+                        data = f.readlines()
+                        f.close()
+
+                    elif "index.htm" in files:
+                        f = open(path + "index.html", "r")
+                        content_type = "text/html"
+                        data = f.readlines()
+                        f.close()
+
+                    else:
+                        self.request.sendall(bytearray(f"{HTTP_404_STATUS}\r\n",'utf-8'))
+                        return
+
+                    data = "".join(data)
+                    self.request.sendall(bytearray(f"{HTTP_200_STATUS}Content-Type: text/html\r\n\r\n{data}",'utf-8'))
+                    return
+
                 elif os.path.isfile(path):
                     # serve file
-                    print("\nIt is a normal file")  
+                    f = open(path, "r")
+                    data = f.readlines()
+                    f.close()
 
-            resources = os.listdir(ROOT + request_string_components[PATH])
+                    data = "".join(data)
 
-            print(resources)
+                    if path.endswith(".css"):
+                        content_type = "text/css"
+                    elif path.endswith(".html"):
+                        content_type = "text/html"
 
-            self.request.sendall(bytearray(f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html></html>",'utf-8'))
+                    self.request.sendall(bytearray(f"{HTTP_200_STATUS}Content-Type: {content_type}\r\n\r\n{data}",'utf-8'))
+                    return
+                
+                else:
+                    self.request.sendall(bytearray(f"{HTTP_500_STATUS}\r\n",'utf-8'))
+                    return
+                    
+            else:
+                self.request.sendall(bytearray(f"{HTTP_404_STATUS}\r\n",'utf-8'))
+                return
+
+            self.request.sendall(bytearray(f"{HTTP_500_STATUS}Content-Type: text/html\r\n\r\n",'utf-8'))
+
         except Exception as e:
             print(e)
-            self.request.sendall(bytearray("HTTP/1.1 500 Internal Server Error\r\n\r\n",'utf-8'))
+            self.request.sendall(bytearray(f"{HTTP_500_STATUS}\r\n",'utf-8'))
 
  
 if __name__ == "__main__":
